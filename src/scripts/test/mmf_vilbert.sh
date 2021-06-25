e3170b369e29bc37a12b74d693c4b24ffda5b404
Hee Ming Shan <mingshan.hee@gmail.com>
2021-06-25 18:25:27 +0700

    Added test scripts

---
+++ src/scripts/test/mmf_vilbert.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=0 mmf_run dataset=hateful_memes \
+    model=vilbert \
+    config=projects/hateful_memes/configs/vilbert/defaults.yaml \
+    checkpoint.resume_file=models/vilbert/vilbert_final.pth \
+    checkpoint.resume_pretrained=False \
+    dataset_config.hateful_memes.annotations.train[0]=hateful_memes/defaults/annotations/train.jsonl \
+    dataset_config.hateful_memes.annotations.val[0]=hateful_memes/defaults/annotations/dev_seen.jsonl \
+    dataset_config.hateful_memes.annotations.test[0]=hateful_memes/defaults/annotations/test_seen.jsonl \
+    run_type=$1
