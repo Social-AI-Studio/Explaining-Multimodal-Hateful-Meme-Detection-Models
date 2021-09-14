3c506c3588a4d3589fa0867aaf590e14ae9e9d5b
Hee Ming Shan <mingshan.hee@gmail.com>
2021-09-15 02:04:58 +0700

    Add pagination for annotation

--- src/client/witbier/annotations/Annotation.vue
+++ src/client/witbier/annotations/Annotation.vue
@@ -1,31 +1,47 @@
 <template>
-  <div class="post">
+  <div class="annotation mb-2 mt-2">
     <div class="loading" v-if="loading">Loading...</div>
     <div v-if="error" class="error">
       {{ error }}
     </div>
     <b-container>
       <b-row cols-md="3">
-        <meme v-for="annotation in annotationList" :key="annotation.id" v-bind:annotation="annotation"></meme>
+        <meme
+          v-for="annotation in annotationList"
+          :key="annotation.id"
+          v-bind:annotation="annotation"
+        ></meme>
       </b-row>
     </b-container>
+
+    <div class="mt-3">
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="rows"
+        align="center"
+        per-page=9
+        hide-goto-end-buttons
+        last-number
+      ></b-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-import { Settings } from '../config/api.config';
-import Meme from './Meme.vue'
-import axios from 'axios';
+import { Settings } from "../config/api.config";
+import Meme from "./Meme.vue";
+import axios from "axios";
 
 export default {
   components: {
-    'meme': Meme
+    meme: Meme,
   },
   data() {
     return {
-      offset: 0,
+      rows: 1,
+      currentPage: 1,
       limit: 9,
-      
+
       loading: false,
       post: null,
       error: null,
@@ -34,27 +50,38 @@ export default {
   created() {
     this.fetchData();
   },
+  watch: {
+    currentPage: function (val, oldVal) {
+      this.fetchData();
+    },
+  },
   methods: {
-    fetchData() {
+    async fetchData() {
       this.error = this.annotationList = null;
       this.loading = true;
 
-      axios
-        .get(`http://${Settings.HOST}:${Settings.PORT}/api/memes/annotations?offset=${this.offset}&limit=${this.limit}`, {
-          headers: {
-            "x-access-token":
-              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjMxNjA5MzU0LCJleHAiOjE2MzE2OTU3NTR9.k6pE6n8OfReqIXIfE4Zu301as9X6V6d2r6DbfMypr-E",
-          },
-        })
-        .then((res) => {
-          this.loading = false;
-          this.annotationList = res.data;
-        })
+      const offset = (this.currentPage - 1) * this.limit;
+
+      const res = await axios
+        .get(
+          `http://${Settings.HOST}:${Settings.PORT}/api/memes/annotations?offset=${offset}&limit=${this.limit}`,
+          {
+            headers: {
+              "x-access-token":
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjMxNjA5MzU0LCJleHAiOjE2MzE2OTU3NTR9.k6pE6n8OfReqIXIfE4Zu301as9X6V6d2r6DbfMypr-E",
+            },
+          }
+        )
         .catch((err) => {
           console.log(err);
         });
-    }
-  }
+
+      this.loading = false;
+      this.rows = res.data.count;
+      this.annotationList = res.data.rows;
+      console.log(this.rows)
+    },
+  },
 };
 </script>
 
