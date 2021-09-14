dc9d08d221d133b067ab2424e9c4fd785a5f9277
Hee Ming Shan <mingshan.hee@gmail.com>
2021-09-15 00:57:45 +0700

    Add basic authentication functionality

---
+++ src/client/witbier/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">SocialAI Studio</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item>
+            <router-link v-if="loggedIn" to="/annotation">Annotation</router-link>
+            <router-link v-if="loggedIn" to="/logout">Log out</router-link>
+            <router-link v-if="!loggedIn" to="/login">Log in</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import auth from "./authentication/auth";
+
+export default {
+  data() {
+    return {
+      loggedIn: auth.loggedIn(),
+    };
+  },
+  created() {
+    auth.onChange = (loggedIn) => {
+      this.loggedIn = loggedIn;
+    };
+  },
+};
+</script>
--- src/client/witbier/app.js
+++ src/client/witbier/app.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+import auth from './authentication/auth'
+import NavBar from './NavBar.vue'
+
+import Login from './authentication/Login.vue'
 import Annotation from './annotations/Annotation.vue'
+
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -12,25 +18,44 @@ Vue.use(IconsPlugin)
 
 Vue.use(VueRouter)
 
+function requireAuth(to, from, next) {
+  if (!auth.loggedIn()) {
+    next({
+      path: '/login',
+      query: { redirect: to.fullPath }
+    })
+  } else {
+    next()
+  }
+}
+
 const Home = { template: '<div>home</div>' }
 const router = new VueRouter({
   mode: 'history',
   base: __dirname,
   routes: [
-    { path: '/', component: Home },
-    { path: '/annotation', component: Annotation },
+    { path: '/login', component: Login },
+    { path: '/annotation', component: Annotation, beforeEnter: requireAuth },
+    {
+      path: '/', component: Home,
+      beforeEnter(to, from, next) {
+        if (!auth.loggedIn()) 
+          next('/login')
+        else
+          next('/annotation')
+      }
+    },
+    {
+      path: '/logout',
+      beforeEnter(to, from, next) {
+        auth.logout()
+        next('/login')
+      }
+    }
   ]
 })
 
 new Vue({
   router,
-  template: `
-    <div id="app">
-      <ul>
-        <li><router-link to="/">/</router-link></li>
-        <li><router-link to="/annotation">Annotations</router-link></li>
-      </ul>
-      <router-view></router-view>
-    </div>
-  `
+  render: h => h(NavBar)
 }).$mount('#app')
